40b6c6483c990b23ce6721babed1e5664e5cdfcd
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2023-10-06 13:13:31 -0300

    Update Desafio DIO 2.py

--- DIO Desafio Basico/Desafio DIO 2.py
+++ DIO Desafio Basico/Desafio DIO 2.py
@@ -1,4 +1,18 @@
-month_dic = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 
+# Desafio
+# Leia um valor inteiro entre 1 e 12, inclusive. Correspondente a este valor, deve ser apresentado como resposta o mês do ano por extenso, em inglês, com a primeira letra maiúscula.
+
+# Entrada
+# A entrada contém um único valor inteiro.
+# Saída
+# Imprima por extenso o nome do mês correspondente ao número existente na entrada, com a primeira letra em maiúscula.
+
+
+
+
+
+
+
+ month_dic = {'January': 1, 'February': 2, 'March': 3, 'April': 4, 'May': 5, 'June': 6, 'July': 7, 
              'August': 8, 'September': 9, 'October': 10, 'November': 11, 'December': 12}
 month = input(' ')
 
