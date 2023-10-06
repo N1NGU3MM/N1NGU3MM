a6b9da5b362d933e0607432dd79e321861d5e948
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2023-10-06 13:15:39 -0300

    Update Desafio DIO 1.py

--- DIO Desafio Basico/Desafio DIO 1.py
+++ DIO Desafio Basico/Desafio DIO 1.py
@@ -1,5 +1,5 @@
 # DESAFIO
-#O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
+# O microblog Twitter é conhecido por limitar as postagens em 140 caracteres. Conferir se um texto vai caber em um tuíte é sua tarefa.
 
 # Entrada
 # A entrada é uma linha de texto T (1 ≤ |T| ≤ 500).
