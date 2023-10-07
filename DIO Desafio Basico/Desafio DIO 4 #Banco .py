c5d43b9173d5a5f50df3a02c23713514d2c0e2d1
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2023-10-06 22:57:24 -0300

    new file:   DIO Desafio Basico/Desafio DIO 4 #Banco .py

---
+++ DIO Desafio Basico/Desafio DIO 4 #Banco .py
@@ -0,0 +1,56 @@
+## Criando um sistema bancário
+
+
+def SISTEMA_BANCARIO():
+    menu = '''
+    d = Depositar
+    s = Sacar
+    e = Extrato
+    q = Sair 
+    '''
+    saldo_da_conta = 1500
+    limite = 500
+    extrato = ''
+    numero_saques = 0
+    LIMITE_SAQUES = 3
+    transacoes = []
+    
+    while True:
+        
+        opção = input(menu)
+        
+        if opção == 'd':
+            print('Depósito')
+            deposito = int(input('Qual o valor para depósito? '))
+            saldo_da_conta += deposito
+            transacoes.append(f'Depósito de R${deposito}')
+            print(f'O valor em seu conta é de R${saldo_da_conta}')
+            
+        elif opção == 's':
+                print('Sacar')
+                valor_sacado = int(input("Qual o valor que deseja sacar? "))
+
+                
+                if numero_saques == LIMITE_SAQUES:
+                    print(f'Limite de {LIMITE_SAQUES} saques diario maximo atingido')
+                    
+                elif valor_sacado <= limite:
+                    if valor_sacado <= saldo_da_conta:
+                        saldo_da_conta -= valor_sacado
+                        numero_saques += 1
+                    print(f'Você sacou o valor de R${valor_sacado}')
+                    transacoes.append(f'Saque de R${valor_sacado}')
+                else:
+                        print('Saldo indisponível. ')
+                        
+
+        
+        elif opção == 'e':
+            print(f'Extrato: R${saldo_da_conta}')
+            print(transacoes )
+
+        elif opção == 'q':
+            break
+        
+        
+SISTEMA_BANCARIO()
