1319dc3756b5cbd6d0663d605d2730ce3ea01e1e
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2023-10-24 13:40:42 -0300

    Update ClassR 2.kt

--- Kotlin/src/ClassR 2.kt
+++ Kotlin/src/ClassR 2.kt
@@ -2,7 +2,7 @@ fun printMessager(message: String): Unit {  // 1. It's an function using for cal
     println(message)  // 2. Remember [fun] is usingo for determined a function 
 }
 
-fun printMessagerWinthPrefix(nessage: String, prefix: String = "info"){ // 3. If not seted an prefiz, the prefix is [info] 
+fun printMessagerWinthPrefix(nessage: String, prefix: String = "info"){ // 3. If not seted an prefix, the prefix is [info] 
     println("[$prefix] $message")
 }
 
@@ -15,7 +15,7 @@ fun multiply(x: Int, y: Int) = x * y // 5. It's a forme of using a function a an
 fun main(){                                            // 6. h
     printMassager("Hello")                            // 7.
     printMessagerWithPrefix("Hello", "Log")           // 8.
-    printlnMessageWithPrefix(prefix = ("Hello")       // 9.
+    printlnMessageWithPrefix(prefix = ("Hello"))       // 9.
     printlnMessageWithPrefix(prefix = "Log", message = "Hello")  // 10. 
     println(sum(1, 2)) 
     printl(multiply(2, 4))    
