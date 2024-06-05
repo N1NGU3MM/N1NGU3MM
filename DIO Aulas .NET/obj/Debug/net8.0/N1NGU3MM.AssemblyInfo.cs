66ef2011a7ff36fc69a79ecf4290dee43374cffb
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2024-06-05 17:32:14 -0300

    Changes to be committed: 	deleted:    AulasC/obj/Debug/net8.0/N1NGU3MM.AssemblyInfoInputs.cache 	renamed:    AulasC/C#.sln -> DIO Aulas .NET/C#.sln 	renamed:    AulasC/Models/Pessoa.cs -> DIO Aulas .NET/Models/Pessoa.cs 	renamed:    AulasC/N1NGU3MM.csproj -> DIO Aulas .NET/N1NGU3MM.csproj 	renamed:    AulasC/Program.cs -> DIO Aulas .NET/Program.cs 	renamed:    AulasC/bin/Debug/net8.0/N1NGU3MM.deps.json -> DIO Aulas .NET/bin/Debug/net8.0/N1NGU3MM.deps.json 	renamed:    AulasC/bin/Debug/net8.0/N1NGU3MM.dll -> DIO Aulas .NET/bin/Debug/net8.0/N1NGU3MM.dll 	renamed:    AulasC/bin/Debug/net8.0/N1NGU3MM.exe -> DIO Aulas .NET/bin/Debug/net8.0/N1NGU3MM.exe 	renamed:    AulasC/bin/Debug/net8.0/N1NGU3MM.pdb -> DIO Aulas .NET/bin/Debug/net8.0/N1NGU3MM.pdb 	renamed:    AulasC/bin/Debug/net8.0/N1NGU3MM.runtimeconfig.json -> DIO Aulas .NET/bin/Debug/net8.0/N1NGU3MM.runtimeconfig.json 	renamed:    AulasC/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs -> DIO Aulas .NET/obj/Debug/net8.0/.NETCoreApp,Version=v8.0.AssemblyAttributes.cs 	renamed:    AulasC/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs -> DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs 	new file:   DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfoInputs.cache 	renamed:    AulasC/obj/Debug/net8.0/N1NGU3MM.GeneratedMSBuildEditorConfig.editorconfig -> DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.GeneratedMSBuildEditorConfig.editorconfig 	renamed:    AulasC/obj/Debug/net8.0/N1NGU3MM.GlobalUsings.g.cs -> DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.GlobalUsings.g.cs 	renamed:    AulasC/obj/Debug/net8.0/N1NGU3MM.assets.cache -> DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.assets.cache 	renamed:    AulasC/obj/N1NGU3MM.csproj.nuget.dgspec.json -> DIO Aulas .NET/obj/N1NGU3MM.csproj.nuget.dgspec.json 	renamed:    AulasC/obj/N1NGU3MM.csproj.nuget.g.props -> DIO Aulas .NET/obj/N1NGU3MM.csproj.nuget.g.props 	renamed:    AulasC/obj/N1NGU3MM.csproj.nuget.g.targets -> DIO Aulas .NET/obj/N1NGU3MM.csproj.nuget.g.targets 	renamed:    AulasC/obj/project.assets.json -> DIO Aulas .NET/obj/project.assets.json 	renamed:    AulasC/obj/project.nuget.cache -> DIO Aulas .NET/obj/project.nuget.cache

--- DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs
+++ DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e51e5e3aa2c0c86a4731f3dec85cc30a52ad858b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+45e0f27c9bf4eb12051d6c6653a6a638a7da86b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
