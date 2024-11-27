5b9eaea836c999164dab6051320aeee4ce9f9bd1
N1NGU3MM <146889623+N1NGU3MM@users.noreply.github.com>
2024-11-27 07:17:36 -0300

    modified:   DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs 	modified:   DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfoInputs.cache 	modified:   DIO Aulas .NET/obj/Debug/net8.0/apphost.exe 	modified:   DIO Aulas .NET/obj/N1NGU3MM.csproj.nuget.dgspec.json 	modified:   DIO Aulas .NET/obj/project.assets.json 	modified:   DIO Aulas .NET/obj/project.nuget.cache 	new file:   DIO Aulas HTML/Aulas HTML/index.html

--- DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs
+++ DIO Aulas .NET/obj/Debug/net8.0/N1NGU3MM.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4fa1b08934bab6aacec4d09fd25a46d8ecf40ba7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2ed92bafb598d6e9e2d871ae2e8bcb1eb0cc19b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyTitleAttribute("N1NGU3MM")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
